606da17cbc2008cf8a7b0a796aaf75119ead519d
samuel-w <67625312+samuel-w@users.noreply.github.com>
2020-06-12 15:50:22 -0500

    Add start scripts

---
+++ start.bat
@@ -0,0 +1,4 @@
+set LIVE_USERNAME=USERNAME_HERE
+set LIVE_PASSWORD=PASSWORD_HERE
+npm run start:desktop
+npm run start:mobile
---
+++ start.sh
@@ -0,0 +1,4 @@
+LIVE_USERNAME=USERNAME_HERE \
+LIVE_PASSWORD=PASSWORD_HERE \
+npm run start:mobile
+npm run start:desktop
